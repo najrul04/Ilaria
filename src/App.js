0dba3f0d9f07f64f0655903ccdde557f092bef7c
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2022-06-26 14:33:54 +0600

    top_part_progressing2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Home from "./pages/home/Home";
 import TopBar from "./topBar/TopBar";
 
 function App() {
   return (
     <>
     <TopBar/>
+    <Home/>
     </>
   );
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../header/Header'
+import "./Home.css"
+
+export default function Home() {
+    return (
+        <div className='home'>
+            <Header />
+            homepage
+        </div>
+    )
+}
