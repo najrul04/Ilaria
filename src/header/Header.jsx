a4438ef5b6e30461787a059b43778607528e8fd8
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2022-06-26 14:46:08 +0600

    top_part_progressing2

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -4,7 +4,11 @@ import "./Header.css"
 export default function Header() {
     return (
         <div className='header'>
-            header
+            <div className="headerTitles">
+                <span className='headerTitleSm'>React & Node</span>
+                <span className='headerTitleLg'>Blog</span>
+            </div>
+            <img src="https://wallpapercave.com/wp/wp6596907.jpg" alt="" className="headerImage" />
         </div>
     )
 }
