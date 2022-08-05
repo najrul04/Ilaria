d384e0eb01ad77bf14cce94c200e5e6473653cb6
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2022-08-05 14:40:16 +0600

    blog_post_Single_Post

--- src/components/SinglePost/SinglePost.jsx
+++ src/components/SinglePost/SinglePost.jsx
@@ -13,6 +13,11 @@ export default function SinglePost() {
                         <i className="singlePostIcon fa-solid fa-trash-can"></i>
                     </div>
                 </hi>
+                <div className="singlePostInfo">
+                  <span className='singlePostAuthor'>Author: <b>Ilaria</b></span>
+                  <span className='singlePostDate'>1 hour ago </span>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nulla repellat libero explicabo optio eaque dolorum ad, laborum mollitia fuga blanditiis, labore est fugit ab sunt delectus quam nam numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque rem eligendi optio nulla sit fugiat necessitatibus hic. Eligendi sit non excepturi a, tenetur fuga dicta, omnis laudantium eos necessitatibus provident ipsa qui, aut expedita consectetur. Voluptatem eaque quibusdam laudantium! Iusto eum nobis illo reiciendis. Impedit atque explicabo itaque illo commodi!</p>
             </div>
         </div>
     )
