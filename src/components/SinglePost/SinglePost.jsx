817e0e477b9a0a7bf797b7881aab7a2d7e7f9287
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2022-08-07 23:03:13 +0600

    added_single_post_css

--- src/components/SinglePost/SinglePost.jsx
+++ src/components/SinglePost/SinglePost.jsx
@@ -1,24 +1,40 @@
-import React from 'react'
-import "./SinglePost.css"
+import React from "react";
+import "./SinglePost.css";
 
 export default function SinglePost() {
-    return (
-        <div className='singlePost'>
-            <div className="singlePostWrapper">
-                <img src="https://wallpapercave.com/dwp1x/wp10415298.jpg" alt="" className="singlePostImage" />
-                <hi className="singlePostTitle">
-                    Lorem ipsum dolor sit
-                    <div className="singlePostEdit">
-                        <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
-                        <i className="singlePostIcon fa-solid fa-trash-can"></i>
-                    </div>
-                </hi>
-                <div className="singlePostInfo">
-                  <span className='singlePostAuthor'>Author: <b>Ilaria</b></span>
-                  <span className='singlePostDate'>1 hour ago </span>
-                </div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nulla repellat libero explicabo optio eaque dolorum ad, laborum mollitia fuga blanditiis, labore est fugit ab sunt delectus quam nam numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque rem eligendi optio nulla sit fugiat necessitatibus hic. Eligendi sit non excepturi a, tenetur fuga dicta, omnis laudantium eos necessitatibus provident ipsa qui, aut expedita consectetur. Voluptatem eaque quibusdam laudantium! Iusto eum nobis illo reiciendis. Impedit atque explicabo itaque illo commodi!</p>
-            </div>
+  return (
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+        <img
+          src="https://wallpapercave.com/dwp1x/wp10415298.jpg"
+          alt=""
+          className="singlePostImage"
+        />
+        <hi className="singlePostTitle">
+          Lorem ipsum dolor sit
+          <div className="singlePostEdit">
+            <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
+            <i className="singlePostIcon fa-solid fa-trash-can"></i>
+          </div>
+        </hi>
+        <div className="singlePostInfo">
+          <span className="singlePostAuthor">
+            Author: <b>Ilaria</b>
+          </span>
+          <span className="singlePostDate">1 hour ago </span>
         </div>
-    )
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nulla
+          repellat libero explicabo optio eaque dolorum ad, laborum mollitia
+          fuga blanditiis, labore est fugit ab sunt delectus quam nam numquam.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque rem
+          eligendi optio nulla sit fugiat necessitatibus hic. Eligendi sit non
+          excepturi a, tenetur fuga dicta, omnis laudantium eos necessitatibus
+          provident ipsa qui, aut expedita consectetur. Voluptatem eaque
+          quibusdam laudantium! Iusto eum nobis illo reiciendis. Impedit atque
+          explicabo itaque illo commodi!
+        </p>
+      </div>
+    </div>
+  );
 }
