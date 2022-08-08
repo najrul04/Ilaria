a58b535e93d9c969bbcc7c5787fcb056540b45c8
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2022-08-08 23:45:14 +0600

    write section done initially

--- src/pages/write/write.jsx
+++ src/pages/write/write.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import "./Write.css"
+import React from "react";
+import "./Write.css";
 export default function Write() {
   return (
-    <div className='write'>
-        <form action="" className='writeForm'>
-          <div className="writeFormGroup">
-            <label htmlFor="fileInput">
-            <i class="fa-regular fa-square-plus"></i>
-            </label>
-            <input type="file" id='fileInput' style={{display:'none'}}/>
-            <input type="text" placeholder='Title' className='writeInput' autoFocus={true}/>
-          </div>
-          <div className="writeFormGroup">
-            <textArea placeholder="Tell Your story..." type="text" className="writeInput writeText"></textArea>
-          </div>
-          <button className="writeSubmit">
-            Publish
-          </button>
-        </form>
+    <div className="write">
+      <img className="writeImage" src="https://wallpapercave.com/dwp1x/wp4800584.jpg" alt="" />
+      <form action="" className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i className="writeIcon fa-solid fa-plus"></i>
+          </label>
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <input
+            type="text"
+            placeholder="Title"
+            className="writeInput"
+            autoFocus={true}
+          />
+        </div>
+        <div className="writeFormGroup">
+          <textArea
+            placeholder="Tell Your story..."
+            type="text"
+            className="writeInput writeText"
+          ></textArea>
+        </div>
+        <button className="writeSubmit">Publish</button>
+      </form>
     </div>
-  )
+  );
 }
