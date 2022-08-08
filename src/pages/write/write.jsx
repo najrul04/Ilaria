8bbaf059cfc633bef925aa58341b4d66e6370b2e
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2022-08-08 18:57:36 +0600

    write section continues

--- src/pages/write/write.jsx
+++ src/pages/write/write.jsx
@@ -3,7 +3,21 @@ import "./Write.css"
 export default function Write() {
   return (
     <div className='write'>
-        Write
+        <form action="" className='writeForm'>
+          <div className="writeFormGroup">
+            <label htmlFor="fileInput">
+            <i class="fa-regular fa-square-plus"></i>
+            </label>
+            <input type="file" id='fileInput' style={{display:'none'}}/>
+            <input type="text" placeholder='Title' className='writeInput' autoFocus={true}/>
+          </div>
+          <div className="writeFormGroup">
+            <textArea placeholder="Tell Your story..." type="text" className="writeInput writeText"></textArea>
+          </div>
+          <button className="writeSubmit">
+            Publish
+          </button>
+        </form>
     </div>
   )
 }
